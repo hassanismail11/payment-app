a16542765b736d576200883f14dab357182933a1
Hassan Ismail <hihassanismail11@gmail.com>
2024-02-04 16:54:02 +0200

    Completed

--- frontend/src/app/Pages/donation-page/donation-page.component.ts
+++ frontend/src/app/Pages/donation-page/donation-page.component.ts
@@ -1,4 +1,4 @@
-import { Component, inject } from '@angular/core';
+import { Component, inject, ViewChild, ElementRef } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import {
   FormsModule,
@@ -9,6 +9,8 @@ import {
 } from '@angular/forms';
 import { CommonModule } from '@angular/common';
 
+import { environment } from '../../../environments/environment.development';
+
 @Component({
   selector: 'app-donation-page',
   standalone: true,
@@ -16,21 +18,64 @@ import { CommonModule } from '@angular/common';
   templateUrl: './donation-page.component.html',
   styleUrl: './donation-page.component.css',
 })
+
+
 export class DonationPageComponent {
+
+  
+  paymentHandler: any = null;
+  stripeAPIKey: any = environment.stripeKey;
+
+  ngOnInit() {
+    this.invokeStripe();
+  }
+
+  invokeStripe() {
+    if (!window.document.getElementById('stripe-script')) {
+      const script = window.document.createElement('script');
+    
+      script.id = 'stripe-script';
+      script.type = 'text/javascript';
+      script.src = 'https://checkout.stripe.com/checkout.js';
+      script.onload = () => {
+        this.paymentHandler = (<any>window).StripeCheckout.configure({
+          key: this.stripeAPIKey,
+          locale: 'auto',
+          token: function (stripeToken: any) {
+            console.log(stripeToken);
+            alert('Payment has been successfull!');
+          },
+        });
+      };
+  
+      window.document.body.appendChild(script);
+    }
+  }
+
+  makePayment(amount: any) {
+    const paymentHandler = (<any>window).StripeCheckout.configure({
+      key: this.stripeAPIKey,
+      locale: 'auto',
+      token: function (stripeToken: any) {
+        console.log(stripeToken);
+        alert('Donation Done Successfully');
+      },
+    });
+    paymentHandler.open({
+      name: 'G I V E .com',
+      description: 'Donation',
+      email: this.form.value.email,
+      amount: amount * 100,
+    });
+  } 
 
   showSuccessMessage = false;
 
   http = inject(HttpClient);
 
   form = new FormGroup({
-    name: new FormControl('', [
-      Validators.required,
-      Validators.minLength(5),
-    ]),
-    email: new FormControl('', [
-      Validators.required,
-      Validators.minLength(5),
-    ]),
+    name: new FormControl('', [Validators.required, Validators.minLength(5)]),
+    email: new FormControl('', [Validators.required, Validators.minLength(5)]),
     phoneNumber: new FormControl('', [
       Validators.required,
       Validators.min(999999999),
@@ -45,17 +90,16 @@ export class DonationPageComponent {
 
   submit() {
     this.showSuccessMessage = false;
-    this.http
-      .post('http://127.0.0.1:8000/cards', this.form.value)
-      .subscribe(
-        res => {
-          console.log(res);
-          this.form.reset();
-          this.showSuccessMessage = true;
-        },
-        err => {
-          console.error(err);
-        }
-      );
+    this.http.post('http://127.0.0.1:8000/cards', this.form.value).subscribe(
+      (res) => {
+        this.makePayment(this.form.value.amount);
+        console.log(res);
+        this.form.reset();
+        this.showSuccessMessage = true;
+      },
+      (err) => {
+        console.error(err);
+      }
+    );
   }
 }
---
+++ frontend/src/environments/environment.development.ts
@@ -0,0 +1,5 @@
+export const environment = {
+  production: false,
+  stripeKey:
+    'pk_test_51OexmOCKCqNZ3b70iNcKskMMgMvpzOerTcX9jC9v1QxA7KQng5aeYy2aBDOfUcrTqCNvQgTYOMKD5WiZQESYWDzG006Omt7ruJ',
+};
